bbefc2f235f5d99c0e1ae1db049406a936bb72c4
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-02-28 11:04:53 -0800

    All bees have been created

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,28 @@
+/*
+.
+├── Grub
+│   └── Bee
+│       ├── HoneyMakerBee
+│       └── ForagerBee
+│          └── RetiredForagerBee
+*/
+
+
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
